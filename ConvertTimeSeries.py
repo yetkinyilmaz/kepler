03374e8ccc5e8d182019ba47dd4da2725d1e5d55
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-02-08 16:26:04 +0100

    commit before abandoning pandas

--- ConvertTimeSeries.py
+++ ConvertTimeSeries.py
@@ -1,26 +1,23 @@
 import numpy as np
 import pandas as pd
 
+
 class AstroData(object):
 
     def __init__(self):
-        self.depth = 5  
+        self.depth = 2
         pass
 
+    def convert(self, orbit=pd.DataFrame()):
 
-    def convert(self, orbit = pd.DataFrame()):
+        timedata = orbit.drop("t", axis=1)
+        for i in range(0, self.depth):
+            #            newcolumn=timedata[i+1:len(timedata)-self.depth+i].reset_index()["x"]
 
-        timedata=orbit.drop("t",axis=1)
-        for i in range(0,self.depth):
-#            newcolumn=timedata[i+1:len(timedata)-self.depth+i].reset_index()["x"]
+            newcolumn = timedata[i + 1:]
+#            newcolumn = newcolumn.reset_index()["position"]
 
-            newcolumn=timedata[i+1:]
-            newcolumn=newcolumn.reset_index()["position"]
-
-
-            timedata = pd.concat([timedata.copy(),newcolumn.copy()],axis=1,ignore_index=True)
+            timedata = pd.concat(
+                [timedata.copy(), newcolumn.copy()], axis=1, ignore_index=True)
 
         return timedata
-
-
-
