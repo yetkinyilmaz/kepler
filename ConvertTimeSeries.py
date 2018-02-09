747fa6a7ae561f615c26554f808232dd0d3a2d0a
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-02-09 16:55:30 +0100

    proper way of building time series with pandas

--- ConvertTimeSeries.py
+++ ConvertTimeSeries.py
@@ -5,19 +5,11 @@ import pandas as pd
 class AstroData(object):
 
     def __init__(self):
-        self.depth = 2
+        self.depth = 3
         pass
 
-    def convert(self, orbit=pd.DataFrame()):
-
-        timedata = orbit.drop("t", axis=1)
-        for i in range(0, self.depth):
-            #            newcolumn=timedata[i+1:len(timedata)-self.depth+i].reset_index()["x"]
-
-            newcolumn = timedata[i + 1:]
-#            newcolumn = newcolumn.reset_index()["position"]
-
-            timedata = pd.concat(
-                [timedata.copy(), newcolumn.copy()], axis=1, ignore_index=True)
-
-        return timedata
+    def convert(self, data):
+        df = pd.DataFrame()
+        for i in np.arange(0, self.depth):
+            df[('t-' + str(i))] = data['position'].shift(i)
+        return df.tail(len(df.index) - self.depth)
