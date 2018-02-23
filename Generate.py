34282abbfcd137978a76d211bb00d245cfc857a1
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-02-23 14:54:52 +0100

    new system, some images

--- Generate.py
+++ Generate.py
@@ -4,7 +4,7 @@ from Transformations import *
 
 class Body(object):
 
-    def __init__(self, x=[], vx=[], i=0, m=1., r=0.00001, lum=1.):
+    def __init__(self, x=[], vx=[], i=0, m=1., r=0.00001, lum=1., spring=1.):
 
         self.id = i
         self.history = np.ndarray(shape=(0, 3))
@@ -15,15 +15,22 @@ class Body(object):
         self.radius = r
         self.luminosity = lum
 
+        self.spring = spring
         self.position[0] = x[0]
         self.position[1] = x[1]
 
-        self.momentum[0] = vx[0]
-        self.momentum[1] = vx[1]
+        self.momentum[0] = m * vx[0]
+        self.momentum[1] = m * vx[1]
 
-    def update(self, acceleration):
-        self.momentum += acceleration
-        self.position += self.momentum
+    def update(self, force):
+        self.momentum += force
+        self.position += self.momentum / self.mass
+        debug = False
+        if(debug):
+            print("position : ", self.position)
+            print("momentum : ", self.momentum)
+            print("force    : ", force)
+
         self.history = np.concatenate(
             (self.history, self.position.reshape(1, -1)))
 
@@ -34,36 +41,85 @@ class System(object):
     def __init__(self):
         self.n_steps = 0
         self.bodies = np.array([])
+        self.g = 1.
+        self.k = 1.
+        self.f = 1.
+
+    def get_phi(self, i):
+        return np.copy(self.cylindrical_relative[i][:, 1])
 
     def force_single_sun(self, p):
-        g = 1.
-        acc = -g * np.array(p.position) / \
+        force = -self.g * p.mass * p.position / \
             pow(np.linalg.norm(p.position), 3)
-        return acc
+        return force
 
     def force_double_sun(self, p):
-        g = 1.
-        acc = np.array([0., 0., 0.])
+        force = np.array([0., 0., 0.])
         suns = [np.array([-50., 0., 0.]),
                 np.array([50., 0., 0.])]
         for sun in suns:
-            acc += -g * np.array(p.position - sun) / \
-                pow(np.linalg.norm(p.position - sun), 3)
-        return acc
+            rel_pos = p.position - sun.position
+            force += -self.g * p.mass * rel_pos / \
+                pow(np.linalg.norm(rel_pos), 3)
+        return force
 
     def force_gravity(self, p):
-        g = 1.
-        acc = np.array([0., 0., 0.])
+        force = np.array([0., 0., 0.])
 
         for body in self.bodies:
             if(p.id != body.id):
-                acc += -g * body.mass * \
-                    np.array(p.position - body.position) / \
-                    pow(np.linalg.norm(p.position - body.position), 3)
-        return acc
+                rel_pos = p.position - body.position
+                force += -self.g * p.mass * body.mass * \
+                    rel_pos / \
+                    pow(np.linalg.norm(rel_pos), 3)
+        return force
 
-    def add_body(self, x=[], v=[], i=0, m=0):
-        p = Body(x, v, i, m)
+    def force_spring(self, p):
+        force = np.array([0., 0., 0.])
+        if(p.id > 0):
+            for body in self.bodies:
+                if(np.abs(p.id - body.id) == 1):
+                    spring = p.spring
+                    if(body.id > p.id):
+                        spring = body.spring
+                    rel_pos = p.position - body.position
+                    dist = np.linalg.norm(rel_pos)
+                    stretch = np.abs(dist - spring)
+                    if(stretch > 0.):
+                        if(stretch < (spring * 0.9)):
+                            force += -self.k * rel_pos *  \
+                                stretch / dist
+#                    print("spring:", spring)
+#                    print("stretch:", stretch)
+#                    print("force:", force)
+        return force
+
+    def force_constant_y(self, p):
+        force = np.array([0., self.f, 0])
+        return force
+
+    def force_pendulum(self, p):
+        force = np.array([0., 0., 0.])
+        if(p.id > 0):
+            force = self.force_spring(p) + self.force_constant_y(p)
+        return force
+
+    def add_body(self, x=[], v=[], m=0., stretch=0.):
+
+        position = np.array(x)
+        velocity = np.array(v)
+
+        while(len(position) < 3):
+            position = np.append(position, 0.)
+        while(len(velocity) < 3):
+            velocity = np.append(velocity, 0.)
+
+        spring = 0.
+        i = len(self.bodies)
+        if(len(self.bodies) > 0):
+            rel_pos = position - self.bodies[i - 1].position
+            spring = np.linalg.norm(rel_pos) + stretch
+        p = Body(position, velocity, i, m, spring=spring)
         self.bodies = np.append(self.bodies, p)
 
     def view(self, iref=0, n_res=1):
@@ -81,10 +137,10 @@ class System(object):
                 self.cartesian_relative[i])
 
     def generate(self, n):
-        self.add_body(x=[60., 62.], v=[0.07, -0.06], i=0, m=0.000001)
-        self.add_body(x=[70., 73.], v=[0.07, -0.07], i=1, m=0.000001)
-        self.add_body(x=[74., 76.], v=[0.075, -0.075], i=2, m=0.000001)
-        self.add_body(x=[174., 176.], v=[0.025, -0.035], i=3, m=0.000001)
+        self.add_body(x=[60., 62.], v=[0.07, -0.06], m=0.000001)
+        self.add_body(x=[70., 73.], v=[0.07, -0.07], m=0.000001)
+        self.add_body(x=[74., 76.], v=[0.075, -0.075], m=0.000001)
+        self.add_body(x=[174., 176.], v=[0.025, -0.035], m=0.000001)
         self.add_body(x=[0., 0.], v=[0., 0.], i=4, m=1.)
 
     def simulate(self, n_steps):
@@ -93,3 +149,4 @@ class System(object):
                 acceleration = self.force(p)
                 p.update(acceleration)
         self.n_steps += n_steps
+
