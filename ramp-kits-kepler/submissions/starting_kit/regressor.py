82277f272ad632971f62e33029eb42eb69fd784c
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2018-04-26 17:49:40 +0200

    LSTM also doesn't crash...

--- ramp-kits-kepler/submissions/starting_kit/regressor.py
+++ ramp-kits-kepler/submissions/starting_kit/regressor.py
@@ -1,6 +1,7 @@
 from sklearn.base import BaseEstimator
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.layers import LSTM
 import numpy as np
 
 
@@ -10,22 +11,21 @@ class Regressor(BaseEstimator):
 
     def fit(self, X, y):
         self.n_sample = X.shape[1]
-        inputs = Input(shape=(self.n_sample,),
+        inputs = Input(shape=(self.n_sample, 1),
                        dtype='float', name='main_input')
-        layer = Dense(60,
-                      activation='relu', kernel_initializer='normal')(inputs)
-        layer = Dense(15,
-                      activation='relu', kernel_initializer='normal')(layer)
-        predictions = Dense(1,
-                            kernel_initializer='normal')(layer)
+        layer = LSTM(8)(inputs)
+        predictions = Dense(1)(layer)
         self.model = Model(inputs=inputs, outputs=predictions)
         self.model.compile(optimizer='adam',
                            loss='mean_squared_error')
-        self.model.fit(X, y, epochs=5, batch_size=1, verbose=2)
+
+        self.model.fit(X.reshape(-1, self.n_sample, 1), y,
+                       epochs=1, batch_size=1, verbose=2)
         print("I did something with fit")
 
     def predict(self, X):
-        y_pred = np.array(self.model.predict(X)).reshape(-1, 1)
+        y_pred = np.array(self.model.predict(
+            X.reshape(-1, self.n_sample, 1))).reshape(-1, 1)
         print("I did something with predict")
 
         return y_pred
@@ -51,10 +51,8 @@ class Regressor2(BaseEstimator):
         self.model.fit(X, y, epochs=5, batch_size=1, verbose=2)
         print("I did something with fit")
 
-    def predict(self, X):
+    def predict2(self, X):
         y_pred = np.array(self.model.predict(X)).reshape(-1, 1)
         print("I did something with predict")
 
         return y_pred
-
-
